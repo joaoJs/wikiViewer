dc888e5bda239920c9e925d5251218f0dea00f14
Joao Campos <j.campos4g@gmail.com>
2017-07-21 18:20:03 -0400

    indentation

--- js/index.js
+++ js/index.js
@@ -25,15 +25,15 @@ $(document).ready(function(){
     $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&generator=allpages&gapfrom="+topic+"&gaplimit=10&origin=*", function(a) {
       
     // json's query property has a pages key wich contains an object for each article.       
-    var ids = a.query.pages;  
-    var pages = Object.keys(ids);
+      var ids = a.query.pages;  
+      var pages = Object.keys(ids);
      
     // for each article object, use its numeric key and its title value to render the links for the ul  
-    pages.forEach(page => {
-      $("#articles").append("<li><a id='listItem' href='https://en.wikipedia.org/?curid="+page+"' target='_blank'>"+ids[page].title+"</a></li>");
-    })                 
+      pages.forEach(page => {
+        $("#articles").append("<li><a id='listItem' href='https://en.wikipedia.org/?curid="+page+"' target='_blank'>"+ids[page].title+"</a></li>");
+      })                 
               
-   });
+    });
     
     // update clear so that links will be removed on next search
     clear = 1;
